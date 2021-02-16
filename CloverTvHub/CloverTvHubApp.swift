2830eac4117766fd4a36621cad76dc826bfc4a4d
otakara <19cm0121@jec.ac.jp>
2021-02-16 14:30:28 +0900

    Initial Commit

---
+++ CloverTvHub/CloverTvHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  CloverTvHubApp.swift
+//  CloverTvHub
+//
+//  Created by 高良 昌辰 on 2021/02/16.
+//
+
+import SwiftUI
+
+@main
+struct CloverTvHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
